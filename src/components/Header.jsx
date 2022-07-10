43fc7e17d76c63afacd13bad237d762ad346473a
botik-spec <62379120+botik-spec@users.noreply.github.com>
2022-07-10 12:04:00 +0300

    Add files via upload

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Header = () => {
+
+    return <header className='header'>
+
+<img src='https://klike.net/uploads/posts/2020-04/1587719791_1.jpg'/> 
+
+header
+    </header >
+
+
+
+}
+export default Header;   
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Navbar = () => {
+
+    return  <nav className='nav'>
+       <div>
+         <a>Profile</a>
+       </div>  
+       <div>
+         <a>message</a>
+       </div>  
+       <div>
+         <a>News</a>
+       </div>  
+       <div>music</div>
+       <div>
+         <a>settngs</a>
+       </div>  
+   </nav>
+
+
+
+} 
+export default Navbar;
